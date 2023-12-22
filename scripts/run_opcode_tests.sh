d4f4867874a2592805bd39a18e80b12b1a17fe4a
Akira Kawata <akirakawata1@gmail.com>
2023-12-22 20:29:02 +0900

    Fix test

--- scripts/run_opcode_tests.sh
+++ scripts/run_opcode_tests.sh
@@ -9,8 +9,8 @@ python3 ./opcode_tests/load_global.py
 python3 ./opcode_tests/fib.py
 python3 ./opcode_tests/hello.py
 python3 ./opcode_tests/use_if.py
-python3 ./opcode_tests/load_method.py
 
 export RAIJIT_TEST_MODE=1
+python3 ./opcode_tests/load_method.py
 python3 ./opcode_tests/unary.py
 python3 ./opcode_tests/opcodes.py
