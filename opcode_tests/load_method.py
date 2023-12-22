b150b58d0e6e2781bc1e26e974852ce53cb7f4fd
Akira Kawata <akirakawata1@gmail.com>
2023-12-22 20:28:51 +0900

    Fix test

--- opcode_tests/load_method.py
+++ opcode_tests/load_method.py
@@ -1,13 +1,13 @@
 import random
 
-def load_method():
+def raijit_test_load_method():
     random.seed(0)
     return random.random()
 
-r0 = load_method()
+r0 = raijit_test_load_method()
 
 import raijit
 raijit.enable()
-r1 = load_method()
+r1 = raijit_test_load_method()
 
-assert r0 == r1
+assert r0 == r1, (r0, r1)
