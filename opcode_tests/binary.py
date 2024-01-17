9e07c630c298829ede82c88cda34a4d8751baff6
Akira Kawata <akirakawata1@gmail.com>
2024-01-17 21:06:39 +0900

    Add inplace add test

--- opcode_tests/binary.py
+++ opcode_tests/binary.py
@@ -21,6 +21,10 @@ def raijit_test_binary_modulo(a, b):
 def raijit_test_binary_pow(a, b):
     return a ** b
 
+def raijite_test_inplace_add(a, b):
+    a += b
+    return a
+
 raijit.enable()
 
 assert raijit_test_binary_and(7, 3) == 3
@@ -31,3 +35,4 @@ assert raijit_test_binary_rshift(7, 3) == 0
 assert raijit_test_binary_modulo(7, 3) == 1
 assert raijit_test_binary_pow(7, 3) == 343
 assert raijit_test_binary_pow(2.0, 1.5) ==  2.0 ** 1.5
+assert raijite_test_inplace_add(7.0, 3.0) == 10.0
