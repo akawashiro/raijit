ae47d5daffb7c3ec53d19291da6a5888800d410a
Akira Kawata <akirakawata1@gmail.com>
2024-01-17 22:29:55 +0900

    Add unit test

--- opcode_tests/copy.py
+++ opcode_tests/copy.py
@@ -1,16 +1,30 @@
 import dis
 
+def raijit_test_unpack_sequence():
+    (a, b) = (1.0, 2.0)
+    return a
+
 def raijit_test_copy():
     pos = [1.0, 2.0]
     pos[0] -= 1.0
     return pos
 
-dis.dis(raijit_test_copy)
+dis.dis(raijit_test_unpack_sequence)
 
-pos1 = raijit_test_copy()
+answer = raijit_test_unpack_sequence()
 
 import raijit
-raijit.enable()
-pos2 = raijit_test_copy()
 
-assert pos1 == pos2
+raijit.enable()
+raijit_result = raijit_test_unpack_sequence()
+raijit.disable()
+
+assert answer == raijit_result
+
+answer = raijit_test_copy()
+
+raijit.enable()
+raijit_result = raijit_test_copy()
+raijit.disable()
+
+assert answer == raijit_result
